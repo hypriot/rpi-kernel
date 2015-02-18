df9b6ca2432dc33e7d34c5479f42ab4691b6fbab
Stefan Scherer <scherer_stefan@icloud.com>
2015-02-18 09:04:01 +0100

    apt-get update

--- scripts/prepare_build_environment.sh
+++ scripts/prepare_build_environment.sh
@@ -1,8 +1,4 @@
-# get the most recent package information
-# if the last update is more than 24h ago
-if [ "$[$(date +%s) - $(stat -c %Z /var/lib/apt/periodic/update-success-stamp)]" -ge 86400 ]; then
-  sudo apt-get update
-fi
+sudo apt-get update
 
 # needed by compile-kernel
 for package in git-core build-essential libncurses5-dev bc tree fakeroot devscripts binfmt-support qemu qemu-user-static debootstrap kpartx lvm2 dosfstools apt-cacher-ng debhelper quilt; do
