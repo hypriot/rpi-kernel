2965abda05da27cf05b268eeaad90d1336add8d9
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-26 21:19:11 +0200

    Add GitHub release

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,17 @@ echo "$CIRCLE_TAG"
 if [ "$CIRCLE_TAG" != "" ]; then
   gem install package_cloud
   package_cloud push Hypriot/rpi/debian/stretch output/*/raspberrypi-kernel*.deb
+
+  curl -sSL https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_linux_amd64.zip -o ghr.zip
+  unzip ghr.zip
+  mkdir ghroutput
+  cp output/*/raspberrypi-kernel*.deb ghroutput/
+
+  if [[ $CIRCLE_TAG = *"rc"* ]]; then
+    pre=-prerelease
+  fi
+  ./ghr $pre -u hypriot $CIRCLE_TAG ghroutput/
+
 else
   echo "No release tag detected. Skip deployment."
 fi
